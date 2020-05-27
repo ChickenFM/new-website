fe6c508d7374fc76f0d1ef86487f3994b2ba8f20
Wisse <wissehes@gmail.com>
2020-05-27 18:01:14 +0200

    Added Error.vue for error handling

---
+++ src/components/Error.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-alert prominent type="error" v-if="show">
+    <v-row align="center">
+      <v-col class="grow">Couldn't connect to the server!.</v-col>
+      <v-col class="shrink">
+        <v-btn @click="reload">Retry</v-btn>
+      </v-col>
+    </v-row>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+      show: Boolean,
+      reload: Function
+  }
+};
+</script>
