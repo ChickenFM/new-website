d0d6323e73db0ff079129e98360ad20255131e76
Wisse <wissehes@gmail.com>
2020-05-27 17:58:36 +0200

    Added navigation drawer with things

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
+    <v-app-bar app color="primary" dark       :clipped-left="$vuetify.breakpoint.lgAndUp">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
+          alt="ChickenFM Logo"
+          class="shrink mr-2 rounded-image"
           contain
           src="./assets/images/cover.png"
           transition="scale-transition"
@@ -12,8 +13,59 @@
         />
       </div>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="true"
+      app
+    >
+      <v-list
+        nav
+        dense
+      >
+            <v-list-item>
+        <v-list-item-content>
+          <v-img 
+          src="@/assets/images/cover.png"
+          class="rounded-image"
+          />
+        </v-list-item-content>
+      </v-list-item>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item tag="router-link" to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
 
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+          <v-list-item tag="router-link" to="/songrequest">
+          <v-list-item-icon>
+            <v-icon>mdi-music-box-multiple</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Song Requests</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item tag="router-link" to="/lyrics">
+          <v-list-item-icon>
+            <v-icon>mdi-music-note</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>Lyrics</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
+      <audio ref="audio"></audio>
       <router-view />
     </v-content>
   </v-app>
@@ -22,8 +74,20 @@
 <script>
 export default {
   name: "App",
-  data: () => ({
-    //
-  })
+    data: () => ({
+      drawer: false,
+      group: null,
+      station: 1
+    }),
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    }
 };
 </script>
+<style scoped>
+.rounded-image {
+    border-radius: 10px;
+}
+</style>
