9cf47dfe52670f649bbd7b430433b361664f44e5
Wisse <wissehes@gmail.com>
2020-06-05 09:46:25 +0200

    Added sendamessage to drawer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark       :clipped-left="$vuetify.breakpoint.lgAndUp">
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+    >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <v-img
@@ -13,54 +18,53 @@
         />
       </div>
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      :clipped="true"
-      app
-    >
-      <v-list
-        nav
-        dense
-      >
-            <v-list-item>
-        <v-list-item-content>
-          <v-img 
-          src="@/assets/images/cover.png"
-          class="rounded-image"
-          />
-        </v-list-item-content>
-      </v-list-item>
+    <v-navigation-drawer v-model="drawer" :clipped="true" app>
+      <v-list nav dense>
+        <v-list-item>
+          <v-list-item-content>
+            <v-img src="@/assets/images/cover.png" class="rounded-image" />
+          </v-list-item-content>
+        </v-list-item>
         <v-list-item-group
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
           <v-list-item tag="router-link" to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
           <v-list-item tag="router-link" to="/songrequest">
-          <v-list-item-icon>
-            <v-icon>mdi-music-box-multiple</v-icon>
-          </v-list-item-icon>
+            <v-list-item-icon>
+              <v-icon>mdi-music-box-multiple</v-icon>
+            </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>Song Requests</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item tag="router-link" to="/lyrics">
-          <v-list-item-icon>
-            <v-icon>mdi-music-note</v-icon>
-          </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Song Requests</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item tag="router-link" to="/lyrics">
+            <v-list-item-icon>
+              <v-icon>mdi-music-note</v-icon>
+            </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>Lyrics</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>Lyrics</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item tag="router-link" to="/sendmessage">
+            <v-list-item-icon>
+              <v-icon>mdi-android-messages</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Send a message</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -74,20 +78,20 @@
 <script>
 export default {
   name: "App",
-    data: () => ({
-      drawer: false,
-      group: null,
-      station: 1
-    }),
-    watch: {
-      group () {
-        this.drawer = false
-      },
+  data: () => ({
+    drawer: false,
+    group: null,
+    station: 1
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
     }
+  }
 };
 </script>
 <style scoped>
 .rounded-image {
-    border-radius: 10px;
+  border-radius: 10px;
 }
 </style>
