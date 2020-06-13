87ed206eb2a158d5d39c5c8bcc2ff8fea80cd144
Wisse <wissehes@gmail.com>
2020-06-13 16:21:52 +0200

    Added translations!

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
+  i18n,
   render: h => h(App)
 }).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,14 @@ module.exports = {
   pwa: {
     name: "ChickenFM",
     themeColor: "#003AFF"
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    }
   }
 };
